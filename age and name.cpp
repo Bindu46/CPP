6f43aac4430372beb0925acc903e82a194e72da3
Bindu46 <b74265601@gmail.com>
2026-03-04 18:05:30 +0530

    Add files via upload

---
+++ age and name.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    string name="Bindu";
+    int age =35;
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    
+    
+    
+    
+}
