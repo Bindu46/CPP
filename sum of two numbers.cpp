8da6705aaac7d2d5da2de2e204a7968d37dd525b
Bindu46 <b74265601@gmail.com>
2026-03-06 18:12:34 +0530

    Add files via upload

---
+++ sum of two numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cout<<"enter the a value"<<endl;
+    cin>>a;
+    cout<<"enter the b value"<<endl;
+    cin>> b;
+    c=a+b;
+    cout<<"sum:"<<c<<endl;
+    
+}
