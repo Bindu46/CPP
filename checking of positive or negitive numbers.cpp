5e02c269eb8af3f42147a2f7c28ac2d2394fe5aa
Bindu46 <b74265601@gmail.com>
2026-03-12 18:27:29 +0530

    Add files via upload

---
+++ checking of positive or negitive numbers.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the  n value";
+    cin>>n;
+    if(n>0)
+    {
+        cout<<"positive";
+    }
+    else if(n<0)
+    {
+        cout<<"negitive";
+    }
+    else 
+    {
+        cout<<"zero";
+    }
+}    
+    
