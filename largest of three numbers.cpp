4b5e54b91e2c1c04c440715ee09acb01b445a875
Bindu46 <b74265601@gmail.com>
2026-03-19 18:16:42 +0530

    Add files via upload

---
+++ largest of three numbers.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the a,b values"<<endl;
+    cin>>a>>b>>c;
+    if(a>b&&a>c)
+    {
+        cout<<" a is large";
+        
+    }
+    else if(b>c)
+    {
+        cout<<"b is large";
+    }
+    else
+    {
+        cout<<"c is large";
+    }
+    
+    
+    
+    
+    
+}
