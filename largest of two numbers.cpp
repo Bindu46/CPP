4410bd7626f16b54e75487ab9263601f721615eb
Bindu46 <b74265601@gmail.com>
2026-03-20 10:15:30 +0530

    Add files via upload

---
+++ largest of two numbers.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the a,b values"<<endl;
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<" a is large";
+        
+    }
+    else
+    {
+        cout<<"b is large";
+    }
+    
+    
+    
+    
+    
+}
