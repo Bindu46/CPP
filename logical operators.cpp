e489280402f2ef442f0bda9ac25d96f55ae21ba8
Bindu46 <b74265601@gmail.com>
2026-03-21 19:30:11 +0530

    Add files via upload

---
+++ logical operators.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n,m;
+     cout<<"enter the n value"<<endl;
+     cin>>n;
+     cout<<"enter the m value"<<endl;
+     cin>>m;
+cout<<"n&&m"<<" "<<(n&&m)<<endl;
+cout<<"n||m"<<" "<<(n||m)<<endl;
+
+}
